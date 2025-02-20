bc711d8ca68d469c47ed39734b3399a60042d41c
kwolny1 <56126333+kwolny1@users.noreply.github.com>
2025-02-21 00:45:44 +0100

    Add framework
    add LogRegCCD.py
    add LogRegCCD_notebook.ipnyb
    create synthetic dataset

---
+++ LogRegCCD.py
@@ -0,0 +1,88 @@
+class LogRegCCD(): 
+    """
+    **DESCRIPTION**
+    """
+
+    def __init__(self): 
+        pass
+
+    def fit(self, X_train, y_train): 
+        """
+        **DESCRIPTION**
+
+        Parameters
+        ----------
+        X_train: 
+
+        y_train: 
+
+        Returns
+        -------
+        self: 
+        """
+        pass
+
+    def validate(self, X_valid, y_valid, measure): 
+        """
+        **DESCRIPTION**
+
+        Parameters
+        ----------
+        X_valid:
+
+        y_valid:
+
+        measure: 
+
+        Returns
+        -------
+        
+
+        """
+        pass
+
+    def predict_proba(self, X_test): 
+        """
+        **DESCRIPTION**
+
+        Parameters
+        ----------
+        X_test: 
+
+        Returns
+        -------
+        
+
+        """
+        pass
+
+
+    def plot_score(self, measure): 
+        """
+        **DESCRIPTION**
+
+        Parameters
+        ----------
+        measure: 
+
+        Returns
+        -------
+        
+
+        """
+        pass
+
+    def plot_coeff(self): 
+        """
+        **DESCRIPTION**
+
+        Parameters
+        ----------
+        X: 
+
+        Returns
+        -------
+        
+
+        """
+        pass
